4c29e02a328ab613ae2f77f8a02a56da8514c4aa
Carpentweet <ashley_carpenter@hotmail.com>
2017-06-08 22:45:14 +0100

    Further work on darts game

--- scripts/game.js
+++ scripts/game.js
@@ -4,10 +4,16 @@ function gameSettings(players, player1name, player2name) {
     noPlayers = players,
     this.player1name = player1name,
     this.player2name = player2name
+    playerturn = 1;
+    turncount = 0;
 }
 
 //Player object
-function player(score) {
+function player(score, name) {
     this.score = score
+    this.name = name
+    this.updateScore = function(roundScore) {
+        this.score -= roundScore;
+    }
 }
 
--- scripts/playgame.js
+++ scripts/playgame.js
@@ -4,8 +4,45 @@ var playerTwo;
 
 //On click of overlay ok button
 $("#okbtn").click(function(){
-    //Update player 1 name
-    $("#player1name").val($("nameone").val());
+    var players;
+    var name1 = $("nameone").val();
+    var name2;
+    var score = $("optionScore").val();
+    playerOne = new player(score, name1);
+    if ($("noplayers1").checked) {
+        players = 1;
+    }
+    else {
+        players = 2;
+        name2 = $("nametwo").val();
+        playerTwo = new player(score, name2);
+    }
+
+    gameSettings = new gameSettings(players, name1, name2);
     //Close the overlay
     $("#overlay").hide("slow");
 })
+
+$("path").click(function(){
+     var clickAttr = $(this).attr("id");
+     var multi = clickAttr.charAt(0);
+     clickAttr = clickAttr.replace(multi, "");
+     var dartScore = parseInt(clickAttr);
+     if(multi = "d") {
+         dartScore = dartScore * 2;
+     }
+     else if(multi == "t") {
+         dartScore = dartScore * 3;
+     }
+
+     if (gameSettings.playerTurn == 1) {
+         updateDartScore(playerOne, dartScore);
+     }
+     else{
+         updateDartScore(playerTwo, dartScore);
+     }
+})
+
+var updateDartScore = function(player, dartScore) {
+    player.updateScore(dartScore);
+}
